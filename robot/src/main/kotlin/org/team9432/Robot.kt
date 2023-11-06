1524f402f4abd80e8bf7598936fe01eab5254fe3
nolan <nolan.s.locke@gmail.com>
2023-11-05 21:22:00 -0700

    disable limelight prints

--- robot/src/main/kotlin/org/team9432/Robot.kt
+++ robot/src/main/kotlin/org/team9432/Robot.kt
@@ -9,8 +9,8 @@ import org.littletonrobotics.junction.Logger
 import org.littletonrobotics.junction.networktables.NT4Publisher
 import org.littletonrobotics.junction.wpilog.WPILOGReader
 import org.littletonrobotics.junction.wpilog.WPILOGWriter
-import org.team9432.lib.drivers.limelight.Limelight
 import org.team9432.lib.commandbased.KCommandScheduler
+import org.team9432.lib.drivers.limelight.Limelight
 import org.team9432.swerve.Controls
 
 
