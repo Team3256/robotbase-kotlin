d6478209539f31785d729a515348d1dba5df1f04
nolan <nolan.s.locke@gmail.com>
2023-11-08 11:30:10 -0700

    add int constructor to point class

--- robot/src/main/kotlin/org/team9432/lib/field/Point.kt
+++ robot/src/main/kotlin/org/team9432/lib/field/Point.kt
@@ -4,7 +4,9 @@ import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.math.geometry.Rotation2d
 import java.awt.geom.Point2D
 
-class Point(x: kotlin.Double, y: kotlin.Double): Point2D.Double(x, y)
+class Point(x: kotlin.Double, y: kotlin.Double): Point2D.Double(x, y) {
+    constructor(x: Int, y: Int): this(x.toDouble(), y.toDouble())
+}
 
 fun Point.toPose2d() = Pose2d(x, y, Rotation2d())
 fun Pose2d.toPoint() = Point(x, y)
