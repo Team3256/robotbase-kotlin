33b9f1304af7fee0b20e331d8a0c951c56affb41
nolan <nolan.s.locke@gmail.com>
2023-11-08 23:01:47 -0700

    make loggerutil accept any kind of collection, not just lists

--- robot/src/main/kotlin/org/team9432/lib/util/LoggerUtil.kt
+++ robot/src/main/kotlin/org/team9432/lib/util/LoggerUtil.kt
@@ -6,7 +6,7 @@ import org.littletonrobotics.junction.Logger
 import org.team9432.lib.field.Point
 
 object LoggerUtil {
-    fun recordPoints(key: String, points: List<Point>) {
+    fun recordPoints(key: String, points: Collection<Point>) {
         val x = points.map { Pose3d(it.x, it.y, 0.0, Rotation3d(0.0, Math.toRadians(-90.0), 0.0)) }.toTypedArray()
         Logger.recordOutput(key, *x)
     }
